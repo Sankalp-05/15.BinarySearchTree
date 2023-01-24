19b80283a5afc655240b90f82e3b494bc394f724
Sankalp <sankalp.kulkarni05@gmail.com>
2023-01-24 12:18:11 +0530

    [Sankalp]-Create a BST from Fig

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,8 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Welcome To Binary Search Tree Program ");
             Console.WriteLine("\n1. Create Binary Search Tree byadding 56 and then adding 30 & 70");
+            Console.WriteLine("2. Create binary tree as show in the figure also show size");
+
             Console.Write("Enter your choice : ");
             int option = Convert.ToInt32(Console.ReadLine());
             BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
@@ -17,6 +19,23 @@ namespace ConsoleApp1
                     binarySearch.Insert(30);
                     binarySearch.Insert(70);
                     binarySearch.Display();
+                    break;
+                case 2:
+                    binarySearch.Insert(30);
+                    binarySearch.Insert(70);
+                    binarySearch.Insert(22);
+                    binarySearch.Insert(40);
+                    binarySearch.Insert(60);
+                    binarySearch.Insert(95);
+                    binarySearch.Insert(11);
+                    binarySearch.Insert(65);
+                    binarySearch.Insert(3);
+                    binarySearch.Insert(16);
+                    binarySearch.Insert(63);
+                    binarySearch.Insert(67);
+                    Console.WriteLine("Binary Search Tree (in inorder traversal) is :");
+                    binarySearch.Display();
+                    binarySearch.GetSize();
                     break;
                 default:
                     Console.WriteLine("Enter valid option!!!");
