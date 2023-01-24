06395e6d450484c59709e0b6cf4a4c61cf7ac3da
Sankalp <sankalp.kulkarni05@gmail.com>
2023-01-24 12:23:40 +0530

    [Sankalp]- Search 63 in BST

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
             Console.WriteLine("Welcome To Binary Search Tree Program ");
             Console.WriteLine("\n1. Create Binary Search Tree byadding 56 and then adding 30 & 70");
             Console.WriteLine("2. Create binary tree as show in the figure also show size");
-
+            Console.WriteLine("3. Search 63 in the Binary Search Tree");
             Console.Write("Enter your choice : ");
             int option = Convert.ToInt32(Console.ReadLine());
             BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
@@ -36,6 +36,25 @@ namespace ConsoleApp1
                     Console.WriteLine("Binary Search Tree (in inorder traversal) is :");
                     binarySearch.Display();
                     binarySearch.GetSize();
+                    break;
+                case 3:
+                    binarySearch.Insert(30);
+                    binarySearch.Insert(70);
+                    binarySearch.Insert(22);
+                    binarySearch.Insert(40);
+                    binarySearch.Insert(60);
+                    binarySearch.Insert(95);
+                    binarySearch.Insert(11);
+                    binarySearch.Insert(65);
+                    binarySearch.Insert(3);
+                    binarySearch.Insert(16);
+                    binarySearch.Insert(63);
+                    binarySearch.Insert(67);
+                    Console.WriteLine("Binary Search Tree (in inorder traversal) is :");
+                    binarySearch.Display();
+                    binarySearch.GetSize();
+                    bool result = binarySearch.IfExists(63, binarySearch);
+                    Console.WriteLine(result);
                     break;
                 default:
                     Console.WriteLine("Enter valid option!!!");
